26060070ef4da738ea360a00dce37e086eead701
NamTranDev <namtran09061992@gmail.com>
2021-04-04 13:18:48 +0700

    - Add Unit Test

---
+++ data/src/test/java/nam/tran/data/interactor/UseCaseTest.kt
@@ -0,0 +1,48 @@
+package nam.tran.data.interactor
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class UseCaseTest {
+
+    @Test
+    fun trendingAndGenre() {
+    }
+
+    @Test
+    fun listTrending() {
+    }
+
+    @Test
+    fun listGenre() {
+    }
+
+    @Test
+    fun listPopular() {
+    }
+
+    @Test
+    fun listTopRated() {
+    }
+
+    @Test
+    fun listUpComing() {
+    }
+
+    @Test
+    fun loadDetail() {
+    }
+
+    @Test
+    fun videoDetail() {
+    }
+
+    @Test
+    fun reviewDetail() {
+    }
+
+    @Test
+    fun recommendationDetail() {
+    }
+}
